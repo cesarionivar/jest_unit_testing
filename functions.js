0ea46b3eae291ecca3c33883afb5c94c0bd988a8
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-20 16:32:17 -0400

    Differents method to compare expecteds values

--- functions.js
+++ functions.js
@@ -1,7 +1,18 @@
+const { default: axios } = require("axios");
+
 const functions = {
 
     add: (num1, num2) => num1 + num2,
-
+    isNUll: () => null,
+    checkValue: x => x,
+    createUser: () => {
+        const user = { firstName: 'Cesario' };
+        user['lastName'] = 'Nivar';
+        return user;
+    },
+    fetchUser: () => axios.get('https://jsonplaceholder.typicode.com/users/1')
+        .then(res => res.data)
+        .catch(err => 'Error')
 }
 
 module.exports = functions;
---
+++ functions.test.js
@@ -0,0 +1,65 @@
+const functions = require('./functions');
+// toBe
+test('Adds 2 + 2 to equal 4', () => {
+    expect(functions.add(2, 2)).toBe(4);
+});
+
+// not
+test('Adds 2 + 2 to not equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
+
+// CHECK FOR TRUTHY & FALSY VALUES
+/* toBeNull matches only null  
+   toBeUndefined matches only undefined
+   toBeDefined is the opposite of toBeUndefined
+   toBeTruthy matches anything that an if statement treats as true
+   toBeFalsy matches anything that an if statement treats as false
+*/
+
+// toBeNull
+test('Should be null', ()=> {
+    expect(functions.isNUll()).toBe(null);
+});
+
+// toBeFalsy
+test('Should be falsy', () => {
+    expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// toSctricEqual
+test('User should be Cesario Nivar object', () => {
+    expect(functions.createUser()).toStrictEqual({
+        firstName: 'Cesario',
+        lastName: 'Nivar'
+    });
+});
+
+// less than and greater than
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+}); 
+
+// Regex
+test('There is no I in Team', () => {
+    expect('team').not.toMatch(/I/i);
+});
+
+// Arrays
+test('Admin should be in usernames', () => {
+    const usernames = ['John', 'Karen', 'Admin'];
+    expect(usernames).toContain('Admin');
+});
+
+
+// Working with async data
+test('User fetched name should be Leanne Graham', () => {
+    expect.assertions(1);
+    return functions.fetchUser()
+        .then(data => {
+            expect(data.name).toEqual('Leanne Graham');
+        });
+});
