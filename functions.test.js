e73bb7489a8e8ed5b4d0c753fd11e3a8a78a5e29
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-03-20 16:36:18 -0400

    Testing Promises

--- functions.test.js
+++ functions.test.js
@@ -56,6 +56,8 @@ test('Admin should be in usernames', () => {
 
 
 // Working with async data
+
+// Promise
 test('User fetched name should be Leanne Graham', () => {
     expect.assertions(1);
     return functions.fetchUser()
@@ -63,3 +65,11 @@ test('User fetched name should be Leanne Graham', () => {
             expect(data.name).toEqual('Leanne Graham');
         });
 });
+
+
+// Async Await
+test('User fetched name should be Leanne Graham', async () => {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toEqual('Leanne Graham');
+}); 
